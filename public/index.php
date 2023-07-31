be4cb4020ce1177f0e7c9122bc76a76a067a4b9e
ymelki <67371192+ymelki@users.noreply.github.com>
2023-07-31 11:33:56 +0200

    mvc

--- public/index.php
+++ public/index.php
@@ -8,8 +8,8 @@
 // si dans l'url il est ecrit /liste_livre alors lance ce code
 // si non erreur
 
-if ($_SERVER['PATH_INFO']=="/accueil"){
-    include __DIR__.'/../src/Controller/Accueil.php';
+if ($_SERVER['PATH_INFO']=="/home"){
+    include __DIR__.'/../src/Controller/HomeController.php';
  // ramener la vue
  // erreur    include __DIR__.'/../templates/accueil.php';
 // a faire dans le controlleur
@@ -17,12 +17,30 @@ if ($_SERVER['PATH_INFO']=="/accueil"){
 }
 
 
-if ($_SERVER['PATH_INFO']=="/accueil_bis"){
-    include __DIR__.'/../templates/accueil.php';
+if ($_SERVER['PATH_INFO']=="/home_bis"){
+    include __DIR__.'/../src/Controller/HomeController.php';
 }
-elseif ($_SERVER['PATH_INFO']=="/liste_livre"){
-    echo "Liste des livres";
+
+// CRUD BOOKS
+elseif ($_SERVER['PATH_INFO']=="/books"){
+    include __DIR__.'/../src/Controller/BookController.php';
+    getBooks();
 }
+
+elseif ($_SERVER['PATH_INFO']=="/one_book"){
+    include __DIR__.'/../src/Controller/BookController.php';
+    getOneBooks();
+}
+
+elseif ($_SERVER['PATH_INFO']=="/remove_book"){
+    include __DIR__.'/../src/Controller/BookController.php';
+}
+
+elseif ($_SERVER['PATH_INFO']=="/insert_book"){
+    include __DIR__.'/../src/Controller/BookController.php';
+}
+
+
 else {
     echo "Erreur 404";
 }
---
+++ src/Controller/BookController.php
@@ -0,0 +1,10 @@
+<?php
+
+// display all books
+function getBooks(){
+    echo "all books ! ";
+}
+// display all books
+function getOneBooks(){
+    echo "One book ! ";
+}
